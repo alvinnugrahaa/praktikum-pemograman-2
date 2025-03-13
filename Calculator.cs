406ae16439a8a1e13f6d5434ef3fb16d9114aad7
alvinnugrahaa <alvinnugraha@students.amikom.ac.id>
2025-03-13 10:02:33 +0700

    Menambahkan fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -5,7 +5,14 @@ namespace CalculatorConsoleApp
     {
         static void Main(string[] args)
         {
-			
+			int a = 10; // declarasi variable a,dg nilai awal 10;
+            int b = 6; // declarasi variable b, dg nilai awal 6;
+
+            Console.WriteLine("Hasil Penambahan " + a + " + " + Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            
+            Console.WriteLine("n\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
 		}  
 		
 		 static int Penambahan(int a, int b)
